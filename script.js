739ed655c87f4fb7a4df4fe8eb52f678517cdc98
Jose Renteria <joserenteria1209@gmail.com>
2020-04-16 20:56:51 -0500

    Added button functionality,had to add functions

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 const container = document.querySelector('#container');
 const buttons = document.querySelectorAll('button');
 let containerSize=16;
-for (i=0;i<containerSize*containerSize;i++){
-    const item = document.createElement('div');
-    item.classList.add("item");
-    container.appendChild(item);
+function createGrid(){
+    container.style.gridTemplateColumns= `repeat(${containerSize}, 1fr)`;
+    container.style.gridTemplateRows= `repeat(${containerSize}, 1fr)`;
+    for (i=0;i<containerSize*containerSize;i++){
+        const item = document.createElement('div');
+        item.classList.add("item");
+        item.style.background= 'white';
+        container.appendChild(item);
+    }
+    childlistener();
+}
+
+function childlistener(){
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => item.addEventListener('mouseover', change));
+
 }
 function change(e) {
-    console.log(e);
     e.target.style.background = 'black';
 
 
 }
 
+function btnclick(e){
+    console.log(e.target.value);
+    const items = document.querySelectorAll('.item');
+    if (e.target.value == "clear"){
+        items.forEach(item => item.style.background = 'white');
+    }
+    else{
+        containerSize= prompt("New Grid Size(only numbers)");
+        console.log(containerSize);
+        items.forEach(item => container.removeChild(item));
+        createGrid(containerSize);
+        }
 
-const items = document.querySelectorAll('.item');
-items.forEach(item => item.addEventListener('mouseover', change));
+    }
+
+
+createGrid(containerSize);
+buttons.forEach(btn => btn.addEventListener('click',btnclick));
