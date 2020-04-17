85c2c8e567b301c6be6357842b478f73c65c4a7f
Jose Renteria <joserenteria1209@gmail.com>
2020-04-17 00:41:57 -0500

    Added random color button functionality, had to rewrite clear function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const container = document.querySelector('#container');
 const buttons = document.querySelectorAll('button');
 let containerSize=16;
+let color=false;
 function createGrid(){
     container.style.gridTemplateColumns= `repeat(${containerSize}, 1fr)`;
     container.style.gridTemplateRows= `repeat(${containerSize}, 1fr)`;
     for (i=0;i<containerSize*containerSize;i++){
         const item = document.createElement('div');
         item.classList.add("item");
+        //item.classList.add('isColored');
         item.style.background= 'white';
         container.appendChild(item);
     }
@@ -19,8 +21,22 @@ function childlistener(){
 
 }
 function change(e) {
-    e.target.style.background = 'black';
-
+    if (color && e.target.classList == "item"){
+        let randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        e.target.style.background = randomColor;
+        e.target.classList.add("isColored");
+        e.target.style.filter= 'brightness(100%)';
+        
+    }
+    else if (!color){
+        e.target.style.background = 'black';
+    }
+    else if ( e.target.classList!== "item"){
+        const filter = getComputedStyle(e.target);
+        let brightness = filter.filter.match(/(\d+)/g);
+        brightness= brightness.join('.');
+        e.target.style.filter = `brightness(${brightness-.1})`
+    }
 
 }
 
@@ -29,16 +45,27 @@ function btnclick(e){
     const items = document.querySelectorAll('.item');
     if (e.target.value == "clear"){
         items.forEach(item => item.style.background = 'white');
+        if(e.target.classList!== "item"){
+            items.forEach(item => { 
+                item.style.filter = 'brightness(1)';
+                item.classList.remove('isColored')});
+
+        }
+    }
+    else if (e.target.value == "changeGrid"){
+        const newSize= prompt("New Grid Size(positive numbers only)");
+        if (!isNaN(parseInt(newSize)) && newSize>0) {
+            containerSize= newSize;
+            console.log(containerSize);
+            items.forEach(item => container.removeChild(item));
+            createGrid(containerSize);
+        }
     }
     else{
-        containerSize= prompt("New Grid Size(only numbers)");
-        console.log(containerSize);
-        items.forEach(item => container.removeChild(item));
-        createGrid(containerSize);
-        }
-
+        items.forEach(item => item.style.background = 'white');
+        color=true;
     }
-
+}
 
 createGrid(containerSize);
 buttons.forEach(btn => btn.addEventListener('click',btnclick));
